3d2e07969689773bceb7af43c98a1f2bf5693bfe
Thomas Boerger <tboerger@tbpro.de>
2013-10-18 16:05:30 +0200

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name "java"
+maintainer "Thomas Boerger"
+maintainer_email "tboerger@tbpro.de"
+license "Apache 2.0"
+description "Installs/Configures java"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version "0.0.1"
+recipe "java", "Installs/Configures java"
+
+supports "suse"
